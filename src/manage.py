c5c88691f86a83ee2787898804af01cb940626d3
Sergei Maertens <sergei@maykinmedia.nl>
2014-02-27 00:15:49 +0100

    Local dev settings

--- src/manage.py
+++ src/manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "langerak_gkv.conf.settings")
 
+
+    for p in sys.path:
+        print p
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
