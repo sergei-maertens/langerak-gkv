1478c72fc1bd019c719ef2634ae584c5cf44747a
Sergei Maertens <sergei@maykinmedia.nl>
2014-04-07 23:48:43 +0200

    Formatting for worklog improved

--- src/langerak_gkv/worklog/models.py
+++ src/langerak_gkv/worklog/models.py
@@ -9,7 +9,16 @@ class LogEntry(models.Model):
     end = models.DateTimeField(_('end'))
     log_message = models.CharField(_('log_message'), max_length=512)
 
+    paid = models.BooleanField(_('paid?'), default=False)
+
     class Meta:
         verbose_name = _(u'log entry')
         verbose_name_plural = _(u'log entries')
         ordering = ('-end', '-start')
+
+    def time_spent(self):
+        seconds = (self.end - self.start).seconds
+        hours = seconds / 3600
+        seconds = seconds - (hours * 3600)
+        minutes = seconds / 60
+        return "{hours}:{minutes}".format(hours=hours, minutes=minutes)
