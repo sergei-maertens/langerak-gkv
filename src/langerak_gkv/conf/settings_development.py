97e34558d8821eb9e6b7cec787761cefcfc6d5ab
sergei-maertens <sergeimaertens@gmail.com>
2014-06-01 03:04:48 +0200

    Initial work on master template

--- src/langerak_gkv/conf/settings_development.py
+++ src/langerak_gkv/conf/settings_development.py
@@ -16,11 +16,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(ROOT_DIR, 'langerak_gkv.db'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'langerak',
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'langerak',
+        'PASSWORD': 'langerak',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
