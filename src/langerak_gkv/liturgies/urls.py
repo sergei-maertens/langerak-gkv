11a8e4f90672f1b95cdd7c01543e47b86b9455b6
Sergei Maertens <sergei@schoolonline.be>
2015-01-22 20:42:24 +0100

    Added liturgies as AppHook and MenuHook

--- src/langerak_gkv/liturgies/urls.py
+++ src/langerak_gkv/liturgies/urls.py
@@ -5,5 +5,6 @@ from .views import liturgy_list, liturgy_detail
 urlpatterns = patterns(
     '',
     url(r'^$', liturgy_list, name='list'),
+    url(r'^list/$', liturgy_list, name='list2'),
     url(r'^(?P<liturgy_id>\d+)/$', liturgy_detail, name='detail'),
 )
