a66f16c476df0af6a9f8e4f4afbb06dcb55dd681
Sergei Maertens <sergei@schoolonline.be>
2015-01-22 20:42:24 +0100

    Added liturgies as AppHook and MenuHook

---
+++ src/langerak_gkv/liturgies/menu.py
@@ -0,0 +1,24 @@
+from django.utils.translation import ugettext_lazy as _
+
+from cms.menu_bases import CMSAttachMenu
+from menus.base import NavigationNode
+from menus.menu_pool import menu_pool
+
+
+class LiturgiesMenu(CMSAttachMenu):
+
+    name = _('liturgies menu')
+
+    def get_nodes(self, request):
+        nodes = []
+        n = NavigationNode(_('sample root page'), "/", 1)
+        n2 = NavigationNode(_('sample settings page'), "/bye/", 2)
+        n3 = NavigationNode(_('sample account page'), "/hello/", 3)
+        n4 = NavigationNode(_('sample my profile page'), "/hello/world/", 4, 3)
+        nodes.append(n)
+        nodes.append(n2)
+        nodes.append(n3)
+        nodes.append(n4)
+        return nodes
+
+menu_pool.register_menu(LiturgiesMenu)
