72bebb706b8bcc980098b772b92b1ea01341483a
Sergei Maertens <sergei@schoolonline.be>
2015-01-22 20:51:25 +0100

    Added history navigation node

--- src/langerak_gkv/liturgies/menu.py
+++ src/langerak_gkv/liturgies/menu.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from cms.menu_bases import CMSAttachMenu
@@ -10,15 +11,9 @@ class LiturgiesMenu(CMSAttachMenu):
     name = _('liturgies menu')
 
     def get_nodes(self, request):
-        nodes = []
-        n = NavigationNode(_('sample root page'), "/", 1)
-        n2 = NavigationNode(_('sample settings page'), "/bye/", 2)
-        n3 = NavigationNode(_('sample account page'), "/hello/", 3)
-        n4 = NavigationNode(_('sample my profile page'), "/hello/world/", 4, 3)
-        nodes.append(n)
-        nodes.append(n2)
-        nodes.append(n3)
-        nodes.append(n4)
-        return nodes
+        return [
+            NavigationNode(_('Liturgy history'), reverse('liturgies:history'), 1)
+        ]
+
 
 menu_pool.register_menu(LiturgiesMenu)
