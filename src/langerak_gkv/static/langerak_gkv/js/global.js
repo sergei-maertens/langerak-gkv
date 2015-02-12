7227a12de1d5dd087b3843ee0066fc0288a0c127
Sergei Maertens <sergei@schoolonline.be>
2015-02-12 21:28:36 +0100

    Header and menu tweaks

--- src/langerak_gkv/static/langerak_gkv/js/global.js
+++ src/langerak_gkv/static/langerak_gkv/js/global.js
@@ -9,7 +9,7 @@ $(function() {
 	$(window).on('map:init', function (e) {
 		var detail = e.originalEvent ? e.originalEvent.detail : e.detail;
 		// add marker
-		console.log(detail.map);
+		// console.log(detail.map);
 		L.marker([FOOTER_MAP_COORDINATES.lat, FOOTER_MAP_COORDINATES.lng]).addTo(detail.map);
 	});
 
