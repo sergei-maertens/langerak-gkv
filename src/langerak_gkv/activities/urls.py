5c2f85435952496ba318665529769a92b58731e0
Sergei Maertens <sergei@maykinmedia.nl>
2015-03-02 23:49:16 +0100

    Styling took some cheating, but week archive view is sort-of okay now

--- src/langerak_gkv/activities/urls.py
+++ src/langerak_gkv/activities/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import ActivityCalendarView, ActivityDetailView
+from .views import ActivityCalendarView, ActivityDetailView, ActivityWeekArchiveView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', ActivityCalendarView.as_view(), name='calendar'),
     url(r'^(?P<slug>[\w\-_]+)/$', ActivityDetailView.as_view(), name='detail'),
+    url(r'^(?P<year>\d{4})/week/(?P<week>\d+)/$', ActivityWeekArchiveView.as_view(), name='week-archive'),
 )
--- src/langerak_gkv/activities/views.py
+++ src/langerak_gkv/activities/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from django.db.models import Q
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, WeekArchiveView
 
 from .models import Activity
 
@@ -13,6 +13,11 @@ class ActivitiesTodayMixin(object):
         context['activities_today'] = Activity.objects.filter(
             Q(start_date=today) | Q(start_date__lt=today, end_date__gte=today)
         )
+        today = date.today()
+        context.update({
+            'this_week': today.strftime('%W'),
+            'this_year': today.year
+        })
         return context
 
 
@@ -23,3 +28,11 @@ class ActivityCalendarView(ActivitiesTodayMixin, TemplateView):
 class ActivityDetailView(ActivitiesTodayMixin, DetailView):
     model = Activity
     context_object_name = 'activity'
+
+
+class ActivityWeekArchiveView(ActivitiesTodayMixin, WeekArchiveView):
+    model = Activity
+    date_field = 'start_date'
+    context_object_name = 'activities'
+    allow_future = True
+    allow_empty = True
