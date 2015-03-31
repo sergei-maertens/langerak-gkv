73fd0bbad1b4f5beabcb5da0dbc82e5578dc1da1
Sergei Maertens <sergei@maykinmedia.nl>
2015-03-31 19:24:57 +0200

    Added popovers to calendar icons

--- src/langerak_gkv/static/langerak_gkv/js/global.js
+++ src/langerak_gkv/static/langerak_gkv/js/global.js
@@ -1,26 +1,29 @@
 $(function() {
 
-	// should get this from the settings but meh
-	FOOTER_MAP_COORDINATES = {
-		lat: 51.9307267,
-		lng: 4.8763104
-	};
+    // should get this from the settings but meh
+    FOOTER_MAP_COORDINATES = {
+        lat: 51.9307267,
+        lng: 4.8763104
+    };
 
-	$(window).on('map:init', function (e) {
-		var detail = e.originalEvent ? e.originalEvent.detail : e.detail;
-		// add marker
-		// console.log(detail.map);
-		L.marker([FOOTER_MAP_COORDINATES.lat, FOOTER_MAP_COORDINATES.lng]).addTo(detail.map);
-	});
+    $(window).on('map:init', function (e) {
+        var detail = e.originalEvent ? e.originalEvent.detail : e.detail;
+        // add marker
+        // console.log(detail.map);
+        L.marker([FOOTER_MAP_COORDINATES.lat, FOOTER_MAP_COORDINATES.lng]).addTo(detail.map);
+    });
 
-	$(window).resize(function() {
-	    $('.link-height').data('processed', false).height('auto');
-	    syncHeights();
-	});
+    $(window).resize(function() {
+        $('.link-height').data('processed', false).height('auto');
+        syncHeights();
+    });
 
+    // popovers
+    $('[data-toggle="popover"]').popover();
     $('.help').popover({
         'placement': 'auto right'
     });
+
 
     $('form.dropdown-menu').on('click', ':not([type="submit"])', function(e) {
         e.preventDefault();
