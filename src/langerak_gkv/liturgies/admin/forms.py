82cb1149b20a27dbf7a2fc4628439865451cdb5e
Sergei Maertens <sergei@maykinmedia.nl>
2015-04-28 09:31:30 +0200

    Styling of the form

--- src/langerak_gkv/liturgies/admin/forms.py
+++ src/langerak_gkv/liturgies/admin/forms.py
@@ -7,6 +7,12 @@ from ..models import MailRecipient
 class LiturgyMailForm(MailForm):
     recipients = forms.ModelMultipleChoiceField(queryset=MailRecipient.objects.filter())
 
+    class Meta(MailForm.Meta):
+        fields = ('recipients', 'subject', 'body')
+        widgets = {
+            'body': forms.Textarea(attrs={'cols': 120, 'rows': 20})
+        }
+
     def __init__(self, *args, **kwargs):
         liturgies = kwargs.pop('liturgies')
         super(LiturgyMailForm, self).__init__(*args, **kwargs)
@@ -14,3 +20,6 @@ class LiturgyMailForm(MailForm):
             liturgy__in=liturgies,
             is_sent=False  # can only send once
         )
+
+    def save(self, *args, **kwargs):
+        import bpdb; bpdb.set_trace()
