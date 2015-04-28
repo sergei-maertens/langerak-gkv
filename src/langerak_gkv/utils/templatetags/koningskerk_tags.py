bd96cab209c37f45e1893ac23592c1cdd5e7d1f4
Sergei Maertens <sergei@maykinmedia.nl>
2015-04-28 22:20:41 +0200

    Added facebook social sharing

--- src/langerak_gkv/utils/templatetags/koningskerk_tags.py
+++ src/langerak_gkv/utils/templatetags/koningskerk_tags.py
@@ -8,3 +8,8 @@ def pop(value, index=0):
     if isinstance(value, list) and len(value) > 0:
         return value.pop(index)
     return None
+
+
+@register.simple_tag(takes_context=True)
+def absolute_url(context, location=None):
+    return context['request'].build_absolute_uri(location)
