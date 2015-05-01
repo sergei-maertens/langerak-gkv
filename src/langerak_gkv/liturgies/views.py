a28d6dd50928717c5d531c42091b1b7af98925bd
Sergei Maertens <sergei@maykinmedia.nl>
2015-05-01 15:54:39 +0200

    Styled the 'upcoming liturgies' page

--- src/langerak_gkv/liturgies/views.py
+++ src/langerak_gkv/liturgies/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.views.generic import ListView, DateDetailView
 
 from langerak_gkv.activities.views import ActivitiesTodayMixin
@@ -7,6 +9,12 @@ from .models import Liturgy
 class LiturgyListView(ActivitiesTodayMixin, ListView):
     model = Liturgy
     template_name = 'liturgies/list.html'
+    context_object_name = 'liturgies'
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = super(LiturgyListView, self).get_queryset()
+        return qs.filter(date__gte=date.today()).order_by('date')
 
 
 class LiturgyDateDetailView(ActivitiesTodayMixin, DateDetailView):
