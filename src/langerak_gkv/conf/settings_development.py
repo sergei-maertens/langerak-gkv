c905d8e7f00f54fa2bc466a6153fa0b5008c85c1
Sergei Maertens <sergei@maykinmedia.nl>
2015-12-19 20:33:10 +0100

    Fixing some settings w/r to migrations

--- src/langerak_gkv/conf/settings_development.py
+++ src/langerak_gkv/conf/settings_development.py
@@ -56,12 +56,12 @@ CSRF_COOKIE_SECURE = False
 #
 # Django debug toolbar
 #
-INSTALLED_APPS += [
-    'debug_toolbar',
-]
-MIDDLEWARE_CLASSES += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
+# INSTALLED_APPS += [
+#     'debug_toolbar',
+# ]
+# MIDDLEWARE_CLASSES += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
 INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False
