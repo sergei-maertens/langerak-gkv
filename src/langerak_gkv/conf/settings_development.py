6bbb5bace0bd6197ae44ad219d805d8ad4e8b8ab
Sergei Maertens <sergei@maykinmedia.nl>
2016-01-04 09:08:55 +0100

    Fix styling of files listed

--- src/langerak_gkv/conf/settings_development.py
+++ src/langerak_gkv/conf/settings_development.py
@@ -51,9 +51,9 @@ CSRF_COOKIE_SECURE = False
 #
 # Django debug toolbar
 #
-# INSTALLED_APPS += [
-#     'debug_toolbar',
-# ]
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
 # MIDDLEWARE_CLASSES += [
 #     'debug_toolbar.middleware.DebugToolbarMiddleware',
 # ]
