4412e37c4d86e53ac36b843d3f59469bb8f865a5
Sergei Maertens <info@regex-it.nl>
2019-03-09 13:57:24 +0100

    Fix search form HTTP 500

--- src/langerak_gkv/search/urls.py
+++ src/langerak_gkv/search/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 
-from haystack.forms import SearchForm
+from haystack.forms import SearchForm as _SearchForm
 from haystack.query import SearchQuerySet
 from haystack.views import SearchView
 
 sqs = SearchQuerySet()
+
+
+class SearchForm(_SearchForm):
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        self.fields['q'].required = True
 
 
 urlpatterns = [
