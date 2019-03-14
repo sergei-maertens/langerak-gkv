44a8875dd8765b202b4298685692a5cb43c732d7
Sergei Maertens <info@regex-it.nl>
2019-03-14 09:23:24 +0100

    :bug: Set correct file uploads permissions

--- src/langerak_gkv/conf/base.py
+++ src/langerak_gkv/conf/base.py
@@ -83,6 +83,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
+FILE_UPLOAD_PERMISSIONS = 0644
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
