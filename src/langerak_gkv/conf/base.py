7f06e123445068db27e45c58fd9b6b339322f3d1
Sergei Maertens <info@regex-it.nl>
2019-12-01 17:18:47 +0100

    :lipstick: Style homegage elements

--- src/langerak_gkv/conf/base.py
+++ src/langerak_gkv/conf/base.py
@@ -1,8 +1,6 @@
 import os
 
-from django.utils.translation import ugettext_lazy as _
-
-from djchoices import ChoiceItem, DjangoChoices
+from .cms import *  # noqa
 
 # Automatically figure out the BASE_DIR and PROJECT_DIR.
 DJANGO_PROJECT_DIR = os.path.abspath(
@@ -297,73 +295,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "/"
-
-
-#
-# Easy thumbnails
-#
-THUMBNAIL_HIGH_RESOLUTION = True
-
-THUMBNAIL_ALIASES = {
-    "": {
-        "default": {"size": (400, 300), "crop": True, "upscale": True},
-        "small": {"size": (120, 120), "crop": True, "upscale": True},
-        "avatar_portrait": {"size": (300, 300), "crop": True, "upscale": True},
-        "header": {"size": (1170, 315), "crop": True, "upscale": True},
-        "birthday": {"size": (50, 50), "crop": True, "upscale": True},
-        "activity_detail": {"size": (800, 400), "crop": True, "upscale": True},
-    }
-}
-THUMBNAIL_ALIASES[""]["homepage"] = THUMBNAIL_ALIASES[""]["default"]
-
-THUMBNAIL_PROCESSORS = (
-    "image_cropping.thumbnail_processors.crop_corners",
-    "easy_thumbnails.processors.colorspace",
-    "easy_thumbnails.processors.autocrop",
-    "filer.thumbnail_processors.scale_and_crop_with_subject_location",
-    "easy_thumbnails.processors.filters",
-)
-
-#
-# Django CMS
-#
-CMS_PERMISSION = True
-
-CMS_TEMPLATES = (
-    ("cms/default.html", _("Default")),
-    ("homepage/home.html", _("Homepage")),
-    ("cms/right_sidebar.html", _("Content left, sidebar right")),
-    ("cms/3_columns.html", _("3 Columns (responsive)")),
-)
-
-CMS_PLACEHOLDER_CONF = {
-    "header_image": {"plugins": ["FilerImagePlugin"]},
-    "slot11": {"plugins": ["HomepageLinkPlugin"]},
-    "slot12": {"plugins": ["HomepageLinkPlugin"]},
-    "slot21": {"plugins": ["HomepageLinkPlugin"]},
-    "slot22": {"plugins": ["HomepageLinkPlugin"]},
-    "preach_image": {"plugins": ["FilerImagePlugin"]},
-    "preach_title": {"plugins": ["CharFieldPlugin"]},
-    "preach_subtitle": {"plugins": ["CharFieldPlugin"]},
-}
-
-DJANGOCMS_PICTURE_ALIGN = (("header", _("header top image")),)
-
-
-class FilerStyles(DjangoChoices):
-    default = ChoiceItem(" ", _("Default"))
-    orange = ChoiceItem("orange", _("Orange"))
-    blue = ChoiceItem("blue", _("Blue"))
-    green = ChoiceItem("green", _("Green"))
-    purple = ChoiceItem("purple", _("Purple"))
-    orange_rm = ChoiceItem("orange read-more", _("Read-more orange"))
-    blue_rm = ChoiceItem("blue read-more", _("Read-more blue"))
-    green_rm = ChoiceItem("green read-more", _("Read-more green"))
-    purple_rm = ChoiceItem("purple read-more", _("Read-more purple"))
-
-
-FILER_LINK_STYLES = FilerStyles.choices
-
 
 #
 # GEO
