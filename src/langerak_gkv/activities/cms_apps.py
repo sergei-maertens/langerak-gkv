83d4f48ef8fd65b65e293efabc0e229be2ed15f0
Sergei Maertens <info@regex-it.nl>
2019-12-30 18:52:06 +0100

    :ok_hand: Tweak header calender items content

--- src/langerak_gkv/activities/cms_apps.py
+++ src/langerak_gkv/activities/cms_apps.py
@@ -6,6 +6,7 @@ from cms.apphook_pool import apphook_pool
 
 class ActivitiesApp(CMSApp):
     name = _("Activities")
+    app_name = "activities"
 
     def get_urls(self, page=None, language=None, **kwargs):
         return ["langerak_gkv.activities.urls"]
