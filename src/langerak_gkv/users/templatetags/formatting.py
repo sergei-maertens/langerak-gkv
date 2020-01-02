84cc562312789c0c1ac899ddc8c03e4cb7186326
Sergei Maertens <info@regex-it.nl>
2020-01-02 09:05:26 +0100

    :lipstick: Improve user profile preview list styling

--- src/langerak_gkv/users/templatetags/formatting.py
+++ src/langerak_gkv/users/templatetags/formatting.py
@@ -4,9 +4,12 @@ register = template.Library()
 
 
 @register.filter
-def phone(phone_nr):
+def phone(phone_nr: str) -> str:
+    if not phone_nr:
+        return ""
+
     try:
         numbers = [char for char in phone_nr if char.isdigit()]
         return "{0}-{1}".format("".join(numbers[:4]), "".join(numbers[4:]))
-    except:
+    except Exception:
         return phone_nr
