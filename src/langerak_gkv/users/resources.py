b2b0f2b2317946e9a7ee3047f4ba79a142f8a4f9
Sergei Maertens <info@regex-it.nl>
2021-04-08 21:51:02 +0200

    :bug: Fix import
    * accept empty mobile numbers
    * accept empty e-mail and empty username -> fall back to UUID

--- src/langerak_gkv/users/resources.py
+++ src/langerak_gkv/users/resources.py
@@ -1,3 +1,5 @@
+import uuid
+
 from import_export import fields, resources
 from import_export.widgets import ForeignKeyWidget
 
@@ -16,6 +18,10 @@ class GetOrCreateForeignKeyWidget(ForeignKeyWidget):
 class UserResource(resources.ModelResource):
     phone = fields.Field(
         attribute="phone",
+        saves_null_values=False,
+    )
+    mobile = fields.Field(
+        attribute="mobile",
         saves_null_values=False,
     )
     district = fields.Field(
@@ -47,7 +53,8 @@ class UserResource(resources.ModelResource):
 
     def before_import_row(self, row, **kwargs):
         if not row["email"]:
-            row["email"] = "{}@koningskerk.nu".format(row["external_code"])
+            ext_code = row["external_code"]
+            row["email"] = "{}@koningskerk.nu".format(ext_code) if ext_code else ""
 
         if not row["username"] and row["email"]:
             # check if we can find the user
@@ -56,6 +63,9 @@ class UserResource(resources.ModelResource):
                 row["username"] = username
             except User.DoesNotExist:
                 row["username"] = row["email"]
+
+        elif not row["username"] and not row["email"]:
+            row["username"] = str(uuid.uuid4())
 
     def before_save_instance(self, instance, using_transactions, dry_run):
         # find the family
