3571a495b6ddfa939daa58ffcd070253430bb90c
Sergei Maertens <info@regex-it.nl>
2022-04-18 15:33:46 +0200

    :white_check_mark: #85 -- added tests for min/max age filters

--- src/langerak_gkv/users/tests/test_member_search.py
+++ src/langerak_gkv/users/tests/test_member_search.py
@@ -1,4 +1,8 @@
+from datetime import date
+
 from django.test import RequestFactory, TestCase
+
+from freezegun import freeze_time
 
 from ..views import UserSearchView
 from .factories import UserFactory
@@ -8,18 +12,43 @@ class UserSearchTests(TestCase):
 
     factory = RequestFactory()
 
-    def test_search_by_partial_family_name(self):
-        user1 = UserFactory.create(last_name="Verstappen")
-        user2 = UserFactory.create(last_name="Hamilton")
-        form_data = {"last_name": "Ham"}
-        request = self.factory.get("/foo", form_data)
-        request.user = user1
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.user = UserFactory.create(exclude_in_queries=True)
+
+    def assertSearchResults(self, params, expected):
+        request = self.factory.get("/foo", params)
+        request.user = self.user
 
         response = UserSearchView.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(
             response.context_data["profiles"],
-            [user2],
+            expected,
             transform=lambda x: x,
         )
+
+    def test_search_by_partial_family_name(self):
+        UserFactory.create(last_name="Verstappen")
+        user2 = UserFactory.create(last_name="Hamilton")
+        query = {"last_name": "Ham"}
+
+        self.assertSearchResults(query, [user2])
+
+    @freeze_time("2022-04-18T13:28:00Z")
+    def test_filter_by_age(self):
+        user1 = UserFactory.create(birthdate=date(1989, 1, 23))
+        user2 = UserFactory.create(birthdate=date(1996, 12, 4))
+
+        with self.subTest("minimum age"):
+            query = {"min_age": 30}
+
+            self.assertSearchResults(query, [user1])
+
+        with self.subTest("maximum age"):
+            query = {"max_age": 27}
+
+            self.assertSearchResults(query, [user2])
