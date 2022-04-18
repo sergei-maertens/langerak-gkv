0883e4924d7bb66f6f1f9d05e2611c6edfdf4a4d
Sergei Maertens <info@regex-it.nl>
2022-04-18 21:27:47 +0200

    :bug: #83 -- set up view to serve login form via AJAX

---
+++ src/langerak_gkv/users/tests/test_login.py
@@ -0,0 +1,15 @@
+from django.test import RequestFactory, TestCase
+
+from ..views import LoginModalView
+
+
+class UserSearchTests(TestCase):
+
+    factory = RequestFactory()
+
+    def test_fetch_login_form_modal_content(self):
+        request = self.factory.get("/foo")
+
+        response = LoginModalView.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
