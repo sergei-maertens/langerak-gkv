e3237c44ef7f8e248ca8af43b24f36d09144dcac
Sergei Maertens <info@regex-it.nl>
2022-04-18 22:57:55 +0200

    :iphone: Improve login experience on mobile

--- src/langerak_gkv/js/components/menu-toggle.js
+++ src/langerak_gkv/js/components/menu-toggle.js
@@ -1,4 +1,4 @@
-const toggle = (control, target, modifier) => {
+const toggle = (control, target, modifier, callback) => {
     const controlNode = document.querySelector(`.${control}`);
     if (!controlNode) {
         return;
@@ -8,16 +8,25 @@ const toggle = (control, target, modifier) => {
 
     controlNode.addEventListener('click', () => {
         targetNode.classList.toggle(toggleClass);
+        callback && callback(targetNode);
     });
 };
 
 const init = () => {
-    toggle('menu__profile', 'user-actions', 'visible');
+    toggle('menu__profile', 'user-actions', 'visible', openLoginModal);
     toggle('menu__search', 'search-form', 'visible');
 
     // profile search
     toggle('profile-list__search-icon', 'profile-list__search', 'opened');
     toggle('profile-list__search-close', 'profile-list__search', 'opened');
+};
+
+const openLoginModal = (targetNode) => {
+    const isVisible = targetNode.classList.contains('user-actions--visible');
+    if (!isVisible) return;
+
+    const loginLink = targetNode.querySelector('[data-modal="login"]');
+    loginLink && loginLink.click();
 };
 
 init();
