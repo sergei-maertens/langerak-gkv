2460631cfecea07a03c2277b75f6af1dd7bb2916
Sergei Maertens <info@regex-it.nl>
2023-10-21 23:11:32 +0200

    :alien: Address breaking changes in Django 4.2

--- src/langerak_gkv/liturgies/signals.py
+++ src/langerak_gkv/liturgies/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from langerak_gkv.activities.models import Activity, ActivityType
 
