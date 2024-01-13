282ed0872d68b61aa493c3ae6d581b62ef2d0815
Sergei Maertens <info@regex-it.nl>
2024-01-13 16:23:15 +0100

    :rocket: [#97] Redo entire Ansible setup using maykin roles/collections

---
+++ src/langerak_gkv/conf/docker.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("DB_CONN_MAX_AGE", "60")
+
+os.environ.setdefault("ENVIRONMENT", "docker")
+os.environ.setdefault("LOG_STDOUT", "yes")
+
+from .production import *
