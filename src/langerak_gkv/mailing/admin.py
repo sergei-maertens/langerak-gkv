a4becd12fb46116ba04d3b138a1881fcb7eac2eb
Sergei Maertens <info@regex-it.nl>
2025-01-19 15:23:43 +0100

    :fire: #104 -- mail templates are entirely obsolete now
    The great cleanup of 2025. If we ever need it again, maykinmedia has
    this in a re-usable django package.

--- src/langerak_gkv/mailing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import MailTemplate
-
-
-@admin.register(MailTemplate)
-class MailTemplateAdmin(admin.ModelAdmin):
-    list_display = ["template_type"]
-    list_filter = ["template_type"]
--- src/langerak_gkv/mailing/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Mail
-
-
-class MailForm(forms.ModelForm):
-    class Meta:
-        model = Mail
-        fields = ("to", "cc", "bcc", "subject", "body")
