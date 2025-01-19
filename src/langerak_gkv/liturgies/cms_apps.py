475f10916bbcfc973dda1578066434fe4029b13d
Sergei Maertens <info@regex-it.nl>
2025-01-19 15:53:30 +0100

    :coffin: Delete more dead code

--- src/langerak_gkv/liturgies/cms_apps.py
+++ src/langerak_gkv/liturgies/cms_apps.py
@@ -3,15 +3,10 @@ from django.utils.translation import gettext_lazy as _
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 
-from .menu import LiturgiesMenu
-
 
 class LiturgiesApp(CMSApp):
     name = _("Liturgies")
     app_name = "liturgies"
-
-    def get_menus(self, page=None, language=None, **kwargs):
-        return [LiturgiesMenu]
 
     def get_urls(self, page=None, language=None, **kwargs):
         return ["langerak_gkv.liturgies.urls"]
--- src/langerak_gkv/utils/templatetags/koningskerk_tags.py
+++ src/langerak_gkv/utils/templatetags/koningskerk_tags.py
@@ -3,37 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter
-def pop(value, index=0):
-    if isinstance(value, list) and len(value) > 0:
-        return value.pop(index)
-    return None
-
-
 @register.simple_tag(takes_context=True)
 def absolute_url(context, location=None):
     return context["request"].build_absolute_uri(location)
-
-
-# @register.inclusion_tag('users/birthdays.html')
-# def show_birthdays(weeks=3):
-#     today = date.today()
-#     upper_limit = today + timedelta(weeks=weeks)
-
-#     month = today.month
-#     qs = User.objects.none()
-#     while month <= upper_limit.month:
-#         qs |= User.objects.filter(birthdate__month=month)
-#         month = (month+1)
-#         if month == 13:
-#             month = 1
-
-#     users = [
-#         user for user in qs.distinct()
-#         if today <= user.birthdate.replace(year=today.year) <= upper_limit
-#     ]
-
-#     return {
-#         'weeks': weeks,
-#         'birthday_users': sorted(users, key=lambda u: (u.birthdate.replace(year=today.year)))
-#     }
