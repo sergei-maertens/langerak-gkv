58f36d276f755cf072caabb5907ae3add9482781
Sergei Maertens <info@regex-it.nl>
2025-10-04 22:20:42 +0200

    :wrench: [#108] Configure settings for Django CMS 4 migration

--- src/langerak_gkv/conf/base.py
+++ src/langerak_gkv/conf/base.py
@@ -179,8 +179,11 @@ INSTALLED_APPS = [
     "django_bleach",
     "solo",
     # cms
-    "cms",
     "treebeard",
+    "cms",
+    "djangocms_4_migration",
+    "djangocms_versioning",
+    "djangocms_alias",
     "djangocms_text_ckeditor",
     "filer",
     "djangocms_file",
